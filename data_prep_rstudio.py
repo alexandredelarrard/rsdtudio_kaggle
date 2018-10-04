5954477a146121f47f2ba83a60f8328cecd0a2a0
alexandre <a.larrard@gmail.com>
2018-10-04 12:01:55 +0200

    data_prep

--- data_prep_rstudio.py
+++ data_prep_rstudio.py
@@ -7,7 +7,29 @@ Created on Thu May 03 13:24:07 2018
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
+import gc
+import time
+from pandas.core.common import SettingWithCopyWarning
+import warnings
+warnings.simplefilter('error', SettingWithCopyWarning)
+gc.enable()
 
-data = pd.read_pickle(r"C:\Users\alexandre\Documents\mxpen\price_elasticity\OW_data_for_nic")
+path = r"C:\Users\alexandre\Documents\kaggle\rstudio\data"
+
+##
+def import_data():
+    
+    train = pd.read_csv(path + "/train.csv", 
+                    dtype={'date': str, 'fullVisitorId': str, 'sessionId':str}, nrows=None)
+    
+    test = pd.read_csv(path + "/test.csv", 
+                   dtype={'date': str, 'fullVisitorId': str, 'sessionId':str}, nrows=None)
+    
+    return train, test
 
 
+if __name__ == "__main__":    
+    train, test = import_data()

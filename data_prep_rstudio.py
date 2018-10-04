60e0c29178a5405deec038ffd27243f8b2892be7
alexandre <a.larrard@gmail.com>
2018-10-04 11:50:40 +0200

    first commit

---
+++ data_prep_rstudio.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 03 13:24:07 2018
+
+@author: alexandre
+"""
+
+import pandas as pd
+import numpy as np
+
+data = pd.read_pickle(r"C:\Users\alexandre\Documents\mxpen\price_elasticity\OW_data_for_nic")
+
+
